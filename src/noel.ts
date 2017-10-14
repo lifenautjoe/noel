807ad05ce95197558aceda886712309df1937cc7
Joel Hernandez <lifenautjoe@gmail.com>
2017-10-14 13:33:15 +0200

    feat(noel): declare class and exported items

--- src/noel.ts
+++ src/noel.ts
@@ -1,4 +1,17 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+
+import { Noel, NoelConfig } from './interfaces';
+
+export class NoelImp implements Noel {
+    constructor(config?: NoelConfig) {}
+}
+
+export * from './errors';
+
+export * from './interfaces';
+
+export * from './types';
+
+export default new NoelImp();
