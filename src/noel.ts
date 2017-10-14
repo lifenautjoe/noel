4919ee0fb42243c2d2ca53af6868605c83ddba67
Joel Hernandez <lifenautjoe@gmail.com>
2017-10-14 14:39:28 +0200

    feat(noel): implement supportedEvents methods

--- src/noel.ts
+++ src/noel.ts
@@ -5,7 +5,29 @@
 import { Noel, NoelConfig } from './interfaces';
 
 export class NoelImp implements Noel {
-    constructor(config?: NoelConfig) {}
+    private supportedEvents: Set<string>;
+
+    constructor(config?: NoelConfig) {
+        config = config || {};
+        const supportedEvents = config.supportedEvents || [];
+        this.setSupportedEvents(supportedEvents);
+    }
+
+    addSupportedEvent(eventName: string) {
+        this.supportedEvents.add(eventName);
+    }
+
+    removeSupportedEvent(eventName: string) {
+        this.supportedEvents.delete(eventName);
+    }
+
+    setSupportedEvents(supportedEvents: Array<string>) {
+        this.supportedEvents = new Set(supportedEvents);
+    }
+
+    eventIsSupported(eventName: string) {
+        return this.supportedEvents.has(eventName);
+    }
 }
 
 export * from './errors';
