b7915976fabafed0ef0dc2259fd1f041fb7fbd68
Joel Hernandez <lifenautjoe@gmail.com>
2017-10-14 15:02:06 +0200

    feat(noel): ad replay methods

--- src/noel.ts
+++ src/noel.ts
@@ -7,9 +7,16 @@ import { Noel, NoelConfig } from './interfaces';
 export class NoelImp implements Noel {
     private supportedEvents: Set<string>;
     private unsupportedEventWarning: boolean;
+    private eventsReplayBuffers: Map<string, Array<any>>;
+
+    private replayEnabled: boolean;
+    private replayBufferAmount: number;
 
     constructor(config?: NoelConfig) {
         config = config || {};
+
+        const replayEnabled = config.replay || false;
+        replayEnabled ? this.enableReplay() : this.disableReplay();
 
         const supportedEvents = config.supportedEvents || [];
         this.setSupportedEvents(supportedEvents);
@@ -39,6 +46,54 @@ export class NoelImp implements Noel {
 
     disableUnsupportedEventWarning() {
         this.unsupportedEventWarning = false;
+    }
+
+    replayIsEnabled() {
+        return this.replayEnabled;
+    }
+
+    enableReplay() {
+        if (this.replayEnabled) return;
+        this.replayEnabled = true;
+        this.clearReplayBuffer();
+    }
+
+    disableReplay() {
+        if (!this.replayEnabled) return;
+        this.replayEnabled = false;
+        this.removeEventsReplayBuffers();
+    }
+
+    setReplayBufferAmount(replayBufferAmount: number) {
+        this.replayBufferAmount = replayBufferAmount;
+        this.trimReplayBuffer(this.replayBufferAmount);
+    }
+
+    getReplayBufferForEvent(eventName: string) {
+        this.eventsReplayBuffers.get(eventName);
+    }
+
+    clearReplayBufferForEvent(eventName: string) {
+        this.eventsReplayBuffers.delete(eventName);
+    }
+
+    clearReplayBuffer() {
+        this.eventsReplayBuffers = new Map();
+    }
+
+    private removeEventsReplayBuffers() {
+        delete this.eventsReplayBuffers;
+    }
+
+    private trimReplayBuffer(replayBufferAmountToTrimTo: number) {
+        this.eventsReplayBuffers.forEach((eventBuffer, eventName) => {
+            const trimmedEventBuffer = eventBuffer.slice(0, replayBufferAmountToTrimTo);
+            this.setReplayBufferForEvent(eventName, trimmedEventBuffer);
+        });
+    }
+
+    private setReplayBufferForEvent(eventName: string, replayBuffer: Array<any>) {
+        this.eventsReplayBuffers.set(eventName, replayBuffer);
     }
 }
 
