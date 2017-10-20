c68ddc1971afe6bc5c5cfdfcffae69c13db88d0c
Joel Hernandez <lifenautjoe@gmail.com>
2017-10-20 15:37:21 +0200

    feat(event-listener-manager): warn if attempt to replay more than buffer can contain

--- src/event-listener-manager.ts
+++ src/event-listener-manager.ts
@@ -2,17 +2,26 @@
  * @author Joel Hernandez <lifenautjoe@gmail.com>
  */
 
-import { NoelEvent, NoelEventListenerManager } from './interfaces';
+import { NoelEventListenerManager } from './interfaces';
 import { NoelEventListener } from './types';
+import { NoelEventImp } from './event';
+import { NoelEventReplayNotEnabled } from './errors';
 
 export class NoelEventListenerManagerImp implements NoelEventListenerManager {
-    constructor(private listener: NoelEventListener, private event: NoelEvent) {}
+    constructor(private listener: NoelEventListener, private event: NoelEventImp) {}
 
     remove() {
         return this.event.removeListener(this.listener);
     }
 
     replay(bufferSize: number): NoelEventListenerManager {
+        const event = this.event;
+        if (!event.getReplayIsEnabled()) throw new NoelEventReplayNotEnabled(event);
+        const actualBufferSize = event.getReplayBufferSize();
+        if (bufferSize > actualBufferSize) {
+            const logger = event.getLogger();
+            logger.warn(`Attempted to replay ${bufferSize} emits from the event "${event.getName()}" but the replay buffer size is set to ${actualBufferSize}.`);
+        }
         const eventReplayBuffer = this.event.getReplayBufferAmount(bufferSize);
         eventReplayBuffer.forEach(eventReplayBufferItem => {
             this.listener(...eventReplayBufferItem);
